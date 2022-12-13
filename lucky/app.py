72c9a906d462dca2d3411e89fc40a7bff056fc69
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 12:35:41 +0900

    text_input

--- lucky/app.py
+++ lucky/app.py
@@ -7,7 +7,10 @@ import numpy as np
 st.title("조 추첨 페이지")
 st.header("여러분의 참여를 환영합니다!")
 
+# https://docs.streamlit.io/library/cheatsheet
+
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input을 만든다.
+st.text_input("조 추첨 대상")
 # 13명이 소속될 조 이름을 넣을 위치를 만든다.
 
 # <추첨 버튼>
