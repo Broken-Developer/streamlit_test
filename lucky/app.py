c48c30a6ee4baf2ead216f60801fd7a64e91e6e4
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 14:19:04 +0900

    tabs

--- lucky/app.py
+++ lucky/app.py
@@ -11,6 +11,9 @@ st.header("여러분의 참여를 환영합니다!")
 
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input을 만든다.
 # 4 X 4 행렬로 배치
+
+tabs = st.tabs(2)
+
 # st.columns(n) : 열을 배치하는 메서드
 columns = st.columns(4) # 화면을 열로 나눠서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
