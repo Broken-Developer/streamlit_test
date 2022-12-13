7fa9120cfeffdf81d84a4d0a592e8bac4d7c6b7d
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 11:45:11 +0900

    streamlit

---
+++ first_webapp/app.py
@@ -0,0 +1,15 @@
+# streamlit 라이브러리 호출
+import streamlit as streamlit
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 제 첫 웹 페이지입니다.
+    ## 부족하지만 많이 사랑해주세요!
+    * 1$ = 1,300원
+    """
+)
+
+st.image(
+    https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_960_720.jpg
+)
