381b173422b88fe070ca5df24d46e49db34d2a5f
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 12:45:19 +0900

    수정

--- lucky/app.py
+++ lucky/app.py
@@ -10,7 +10,18 @@ st.header("여러분의 참여를 환영합니다!")
 # https://docs.streamlit.io/library/cheatsheet
 
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input을 만든다.
-st.text_input("조 추첨 대상")
+# 3 X 4 행렬로 배치
+# 열을 배치하는 메서드
+columns = st.columns(4) # 화면을 열로 나눠서 배치
+# 가로 4개의 열 -> columns = [col1, col2, col3, col4]
+# col1, col2, col3, col4로 언팩킹해서 사용해도 된다.
+# 우리는 for문을 사용한다.
+for idx, col in enumerate(columns) :
+    # col.text_input(f"조 추첨 대상 {idx + 1}", key="idx")
+    for idx2 in range(4) :
+        # 키가 겹치면 안된다.
+        col.text_input(f"조 추첨 대상 {idx + idx2 * 4}", key="idx + idx2 * 4")
+
 # 13명이 소속될 조 이름을 넣을 위치를 만든다.
 
 # <추첨 버튼>
