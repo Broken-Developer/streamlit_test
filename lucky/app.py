f83d017f88abdbc5c469cc974985a960d3d512f7
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 14:24:46 +0900

    columns2 추가(조)

--- lucky/app.py
+++ lucky/app.py
@@ -17,7 +17,7 @@ tabs = st.tabs(['참가자'])
 # st.columns(n) : 열을 배치하는 메서드
 # columns = st.columns(4) # 화면을 열로 나눠서 배치
 
-# 0번째 탭에 컬럼(열)을 넣겠다.
+# 0번째 탭에 참가자 컬럼(열)을 넣겠다.
 columns = tabs[0].columns(4)
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4로 언팩킹해서 사용해도 된다.
@@ -30,6 +30,20 @@ for idx, col in enumerate(columns) : # 열의 위치
         # 키가 겹치면 안된다.
         # col 안에 메서드를 통해서 요소들을 생성해주겠다는 의미
         col.text_input(f"조 추첨 대상 {idx + 1 + idx2 * 4}", key=f"n{idx + 1 + idx2 * 4}")
+
+# 2번째 탭에 조 컬럼(열)을 넣겠다.
+columns2 = tabs[1].columns(4)
+# 가로 4개의 열 -> columns = [col1, col2, col3, col4]
+# col1, col2, col3, col4로 언팩킹해서 사용해도 된다.
+
+# 우리는 for문을 사용한다.
+for idx, col in enumerate(columns2) : # 열의 위치
+    # enumerate() : index와 value의 묶음
+    # col.text_input(f"조 추첨 대상 {idx + 1}", key="idx")
+    for idx2 in range(4) :
+        # 키가 겹치면 안된다.
+        # col 안에 메서드를 통해서 요소들을 생성해주겠다는 의미
+        col.text_input(f"조 목록 {idx + 1 + idx2 * 4}", key=f"g{idx + 1 + idx2 * 4}")
 
 # 13명이 소속될 조 이름을 넣을 위치를 만든다.
 st.write(st.session_state)
