66da0bae2d636eb58a1419b86c780963eeaf4fdf
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 12:46:25 +0900

    수정

--- lucky/app.py
+++ lucky/app.py
@@ -20,7 +20,7 @@ for idx, col in enumerate(columns) :
     # col.text_input(f"조 추첨 대상 {idx + 1}", key="idx")
     for idx2 in range(4) :
         # 키가 겹치면 안된다.
-        col.text_input(f"조 추첨 대상 {idx + idx2 * 4}", key="idx + idx2 * 4")
+        col.text_input(f"조 추첨 대상 {idx + idx2 * 4}", key=f"{idx + idx2 * 4}")
 
 # 13명이 소속될 조 이름을 넣을 위치를 만든다.
 
