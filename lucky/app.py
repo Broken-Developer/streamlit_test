77c865b31df57264313956d4156c3c6b87cadce1
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 14:36:59 +0900

    n_data 섞기

--- lucky/app.py
+++ lucky/app.py
@@ -56,6 +56,7 @@ ss = pd.Series(st.session_state) # 딕셔너리 -> 시리즈
 # ss2 = ss[ss != ""]
 ss2 = ss[ss.ne("")]
 st.write(ss2)
+
 # string과 관련된 메서드를 사용할 수 있게 한다.
 n_idx = ss2.index.str.contains('n')
 n_data = ss2[n_idx]
@@ -64,6 +65,9 @@ st.write(n_data)
 g_idx = ss2.index.str.contains('g')
 g_data = ss2[g_idx]
 st.write(g_data)
+
+# n_data를 섞어준다(비복원으로)
+np.random.choice(n_data, len(n_data), replace=False)
 
 # 2. df형태로 분리
 
