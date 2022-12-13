b79bce76d1e66e5285da05892d1bf20ba479ebc6
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-13 15:21:55 +0900

    titanic

--- graph/app.py
+++ graph/app.py
@@ -4,4 +4,24 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
-st.write('hello')
+st.write(
+    '''
+    ## 탐색적 데이터 분석(EDA)과 Seaborn을 통한 시각화
+    ## 데이터의 종류
+    * 정형 데이터 : 값으로 나타낼 수 있는 데이터(숫자)
+    * 비정형 데이터 : 정형 데이터가 아닌 나머지 데이터(사진, 언어, ...)
+    ## 정형 데이터
+    |대분류|소분류|예시|
+    |:-|:-|:-|
+    |수치형 데이터<br>(사칙 연산이 가능한 데이터)|연속형 데이터|키, 몸무게, 수입
+    ||이산형 데이터|과일 개수, 책의 페이지 수
+    |범주형 데이터<br>(범주로 나누어지는 데이터)|순서형 데이터|학점, 순위(랭킹)|
+    ||명목형 데이터|성별, 음식종류, 우편번호|
+    # 수치형 데이터 numberical data
+    > 사칙 연산이 가능한 데이터(+, -, *, /)
+    '''
+)
+
+# df
+titanic = sns.load_dataset('titanic')
+st.write(titanic) # 적당히 잘라줌
