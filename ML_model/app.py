4d89108bf23406b91ac7574898d1e831de46f7db
Broken-Developer <119405312+Broken-Developer@users.noreply.github.com>
2022-12-15 17:17:54 +0900

    pkl 파일명 변경

--- ML_model/app.py
+++ ML_model/app.py
@@ -9,7 +9,7 @@ import joblib
 import os
 # os.path... 파이썬 경로문제
 
-model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
